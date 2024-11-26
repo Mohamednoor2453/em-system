d98db53b100a47e05999bfd9fed51a2774234f9c
Hp <hafsanoor401@gmail.com>
2024-11-26 19:59:54 +0300

    ems frontend

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const isAuthenticated = require('../middleware/authMiddleware.js');
 // const isAdmin = require('../middleware/adminMiddleware.js');
+router.get('/', (req, res) => {
+    res.render('home');
+});
 
 router.get('/login', (req, res) => {
     res.render('login');
@@ -13,6 +16,16 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.get('/employee', (req, res) => {
+    res.render('employee');
+});
+
+router.get('/leaves', (req, res) => {
+    res.render('leave');
+});
+router.get('/hr', (req, res) => {
+    res.render('humanResource');
+});
 router.get('/addEmployee', (req, res) => {
     res.render('addEmployee');
 });
